6445643cb065f87ab9f69202e2f8d57457ab34e3
Code90AAP <anderson.kol9090@gmial.com>
2025-06-11 20:15:00 -0500

    Corrigiendo cambio anterior 2

--- proyecto_pytest/rum_tests.sh
+++ proyecto_pytest/rum_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Activando el entorno virtual"
-source venv/bin/activate
+source venv/Scripts/activate || exit 1
 
 echo "Instalando dependencias"
-pip install -r requirements.txt
+pip install -r requirements.txt || exit 1
 
 echo "Ejecutando pruebas con pytest"
-pytest tests/ --junitxml=reports/test_results.xml --html=reports/test_results.html --self-contained-html
+pytest tests/ --junitxml=reports/test_results.xml --html=reports/test_results.html --self-contained-html exit 1
 
 echo "Pruebas completadas. Resultados guardados en reports/test_results.xml y reports/test_results.html"
