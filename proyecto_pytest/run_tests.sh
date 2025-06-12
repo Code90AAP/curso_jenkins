534cdbf9c7aa38e70153053c1415a11e53385369
Code90AAP <anderson.kol9090@gmial.com>
2025-06-11 20:16:52 -0500

    Corrigiendo cambio anterior 3

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -7,6 +7,6 @@ echo "Instalando dependencias"
 pip install -r requirements.txt || exit 1
 
 echo "Ejecutando pruebas con pytest"
-pytest tests/ --junitxml=reports/test_results.xml --html=reports/test_results.html --self-contained-html exit 1
+pytest tests/ --junitxml=reports/test_results.xml --html=reports/test_results.html --self-contained-html || exit 1
 
 echo "Pruebas completadas. Resultados guardados en reports/test_results.xml y reports/test_results.html"
